6dc13ba7195218c656ab0002d8b2ec6da26a53e0
lhj <1871731528@qq.com>
2021-03-05 15:34:25 +0800

    vue-element-admin

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,11 @@
 <template>
   <div id="app">
-    <!-- <router-view/> -->
-    <el-form inline :model="data">
-      <el-form-item label="审批人">
-        <el-input v-model="data.user" placeholder="审批人"></el-input>
-      </el-form-item>
-      <el-form-item label="活动区域">
-        <el-select v-model="data.region" placeholder="活动区域">
-          <el-option label="区域一" value="shanghai"></el-option>
-          <el-option label="区域二" value="beijing"></el-option>
-        </el-select>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="onSubmit">查询</el-button>
-      </el-form-item>
-    </el-form>
+    <router-view />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App',
-  data() {
-    return {
-      data: {
-        user: 'sam',
-        region: '区域二',
-      },
-    };
-  },
-  methods: {
-    onSubmit() {
-      console.log(this.data);
-    },
-  },
-};
+  name: 'App'
+}
 </script>
-
-<style lang="stylus">
-</style>
